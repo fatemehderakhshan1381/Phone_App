19f1e39ccc0010a029ca52c936664f96aed4bd8f
fatemehderakhshan1381 <fatemehderakhshan20@gmail.com>
2023-06-08 14:11:35 +0430

    create app

---
+++ app/src/main/java/com/uni/phoneapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.uni.phoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //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
+    }
+}
